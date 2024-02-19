91a02b24f5c6b090974c40b48131cda80ed9b36a
unknown <kaylatran2021@gmail.com>
2024-02-19 01:38:33 -0800

    feat: ROUGHUI barebones main

---
+++ app/(tabs)/main.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "@/components/Themed";
+const map = require('@/assets/images/map.jpg');
+
+export default function MainScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={map}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
