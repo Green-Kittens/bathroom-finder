48bae159e9d49df54e1e2a1c09c9ca47af3e8ba1
Jonas Schiessl <j.schiessl2001@gmail.com>
2024-02-26 14:42:24 -0800

    Update Forgot Password text in login screen(Typo)

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import { StyleSheet,Image, SafeAreaView, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { Text, View } from "@/components/Themed";
 
 function TabLoginScreen() {
   // State management for text inputs
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const onLoginPress = () => {
     // Placeholder for navigation logic
-    Alert.alert('Login Pressed', 'Navigate to home screen.');
+    Alert.alert("Login Pressed", "Navigate to home screen.");
   };
 
   const onRegisterPress = () => {
     // Placeholder for navigation logic
-    Alert.alert('Register Pressed', 'Navigate to registration screen.');
+    Alert.alert("Register Pressed", "Navigate to registration screen.");
   };
 
   const onForgotPress = () => {
     // Placeholder for navigation logic
-    Alert.alert('Frogot Pressed', 'Navigate to Frogot screen.');
+    Alert.alert("Forgot Pressed", "Navigate to Forgot screen.");
   };
 
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
-        <Image source ={require('../../assets/images/logo-placeholder.png')} style = {styles.logo} />
+        <Image
+          source={require("../../assets/images/logo-placeholder.png")}
+          style={styles.logo}
+        />
         <Text style={styles.title}>Login</Text>
 
         {/* Text input fields */}
@@ -42,27 +53,33 @@ function TabLoginScreen() {
           placeholder="Password"
           secureTextEntry={true}
         />
-        <TouchableOpacity onPress={onForgotPress} style={styles.registerContainer}>
-        <Text style={styles.text}>Forgot Password</Text>
+
+        {/* Forgot Password Section */}
+        <TouchableOpacity
+          onPress={onForgotPress}
+          style={styles.registerContainer}
+        >
+          <Text style={styles.text}>Forgot Password</Text>
         </TouchableOpacity>
 
         {/* Login button */}
         <View style={styles.fixToText}>
-          <Button
-            title="Login"
-            color={'#000000'}
-            onPress={onLoginPress} 
-          />
+          <Button title="Login" color={"#000000"} onPress={onLoginPress} />
         </View>
       </SafeAreaView>
 
       {/* Footer with Register Section */}
       <View style={styles.footer}>
-        <View style ={styles.registerContainer}>
-          <Text style={styles.text} disabled>Don't have an account? </Text>
-            <TouchableOpacity onPress={onRegisterPress} style={styles.registerContainer}>
-              <Text style={styles.registerText}>Register</Text>
-            </TouchableOpacity>
+        <View style={styles.registerContainer}>
+          <Text style={styles.text} disabled>
+            Don't have an account?{" "}
+          </Text>
+          <TouchableOpacity
+            onPress={onRegisterPress}
+            style={styles.registerContainer}
+          >
+            <Text style={styles.registerText}>Register</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -75,60 +92,60 @@ const styles = StyleSheet.create({
   // Main Container
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   // Safe Area Section
   safeArea: {
     flex: 1,
-    alignItems: 'center',  
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   // Title Section
-  title: {              
+  title: {
     fontSize: 20,
-    fontWeight: 'bold',  
+    fontWeight: "bold",
     marginBottom: 50,
   },
   // Input Section
   input: {
     height: 40,
-    width: '30%',  // Control the width of the input size
+    width: "30%", // Control the width of the input size
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    alignSelf: 'center',  
+    alignSelf: "center",
   },
   // Text Section
-  text: {                   
-    alignSelf: 'center',
+  text: {
+    alignSelf: "center",
   },
   // Button Section
-  fixToText: {                    
+  fixToText: {
     marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   // Footer Section
-  footer: {                 
+  footer: {
     paddingBottom: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   // Register Section
-  registerContainer: {          
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+  registerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   // Register Text
   registerText: {
-    color: 'black',
-    fontWeight: 'bold',
+    color: "black",
+    fontWeight: "bold",
   },
   // Logo
   logo: {
     width: 100,
     height: 100,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginBottom: 5,
-  }
+  },
 });
