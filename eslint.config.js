bffbec3edf007020df99dc4ffabdee246fa6ed16
Dylan Tompkins <dltompki@outlook.com>
2024-02-28 00:52:22 -0800

    fix: update eslint config to actually get it to fail

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
+// @ts-check
+
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [eslintPluginPrettierRecommended, reactRecommended];
+export default [
+  { ignores: [".expo"] },
+  eslintPluginPrettierRecommended,
+  reactRecommended,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+];
