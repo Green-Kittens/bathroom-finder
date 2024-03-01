344eb73826a0e5c15f76bbd0c9ffa0d3dcede977
lindlands <lindlands@outlook.com>
2024-03-01 15:54:26 -0800

    update login colors, + background image

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   Image,
+  ImageBackground,
   SafeAreaView,
   TextInput,
-  Button,
   Alert,
   TouchableOpacity,
+  Pressable,
 } from "react-native";
 import { Text, View } from "@/components/Themed";
+import { LinearGradient } from "expo-linear-gradient";
+
+const boomerangimage = { uri: "/assets/images/boomerang.png" };
 
 function TabLoginScreen() {
   // State management for text inputs
@@ -27,11 +31,25 @@ function TabLoginScreen() {
 
   const onForgotPress = () => {
     // Placeholder for navigation logic
-    Alert.alert("Frogot Pressed", "Navigate to Frogot screen.");
+    Alert.alert("Forgot Pressed", "Navigate to Forgot screen.");
   };
 
   return (
     <View style={styles.container}>
+      <ImageBackground
+        source={boomerangimage}
+        style={{
+          width: 753,
+          height: 499,
+          position: "absolute",
+          top: 0,
+          left: -200,
+        }}
+        imageStyle={{
+          resizeMode: "cover",
+          alignSelf: "flex-end",
+        }}
+      ></ImageBackground>
       <SafeAreaView style={styles.safeArea}>
         <Image
           source={require("../../assets/images/logo-placeholder.png")}
@@ -44,7 +62,7 @@ function TabLoginScreen() {
           style={styles.input}
           onChangeText={setUsername}
           value={username}
-          placeholder="Username:"
+          placeholder="Username"
         />
         <TextInput
           style={styles.input}
@@ -61,9 +79,16 @@ function TabLoginScreen() {
         </TouchableOpacity>
 
         {/* Login button */}
-        <View style={styles.fixToText}>
-          <Button title="Login" color={"#000000"} onPress={onLoginPress} />
-        </View>
+        <LinearGradient
+          // Button Linear Gradient
+          colors={["#6da798", "#40a4a9"]}
+          end={{ x: 0.1, y: 0.2 }}
+          style={styles.button}
+        >
+          <Pressable style={styles.button} onPress={onLoginPress}>
+            <Text style={[styles.text, { fontWeight: "bold" }]}>{"Login"}</Text>
+          </Pressable>
+        </LinearGradient>
       </SafeAreaView>
 
       {/* Footer with Register Section */}
@@ -91,6 +116,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between",
+    backgroundColor: "#EEF8F7",
   },
   // Safe Area Section
   safeArea: {
@@ -112,6 +138,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     alignSelf: "center",
+    backgroundColor: "#FFFFFF",
+    minWidth: 200,
   },
   // Text Section
   text: {
@@ -127,12 +155,14 @@ const styles = StyleSheet.create({
   footer: {
     paddingBottom: 20,
     alignItems: "center",
+    backgroundColor: "none",
   },
   // Register Section
   registerContainer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "none",
   },
   // Register Text
   registerText: {
@@ -145,5 +175,12 @@ const styles = StyleSheet.create({
     height: 100,
     alignSelf: "center",
     marginBottom: 5,
+  },
+  button: {
+    justifyContent: "center",
+    alignSelf: "center",
+    borderRadius: 4,
+    paddingHorizontal: 20,
+    margin: 10,
   },
 });
